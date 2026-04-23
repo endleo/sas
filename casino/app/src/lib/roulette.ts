511587fe74cdda483815efbebb819ae5e8462d6d
Leonhard Ender <e12027408@student.tuwien.ac.at>
2026-04-23 04:38:18 +0200

    rudimentary roulette implementation

--- casino/app/src/lib/roulette.ts
+++ casino/app/src/lib/roulette.ts
@@ -1,4 +1,4 @@
-const fields = [
+export const fields = [
   { nr: 0, color: "green" },
   { nr: 1, color: "red" },
   { nr: 2, color: "black" },
@@ -38,62 +38,87 @@ const fields = [
   { nr: 36, color: "red" },
 ];
 
-const bets = {
+interface Bet {
+  payout: number;
+  winningRolls: number[];
+}
+
+export const bets: { [key: string]: Bet } = {
   even: {
-    payout: 1,
+    payout: 2,
     winningRolls: fields
       .filter((field) => field.nr % 2 == 0 && field.nr != 0)
       .map((field) => field.nr),
   },
   odd: {
-    payout: 1,
+    payout: 2,
     winningRolls: fields
       .filter((field) => field.nr % 2 == 1)
       .map((field) => field.nr),
   },
   red: {
-    payout: 1,
+    payout: 2,
     winningRolls: fields
       .filter((field) => field.color == "red")
       .map((field) => field.nr),
   },
   black: {
-    payout: 1,
+    payout: 2,
     winningRolls: fields
       .filter((field) => field.color == "black")
       .map((field) => field.nr),
   },
   low: {
-    payout: 1,
+    payout: 2,
     winningRolls: fields
       .filter((field) => field.nr >= 1 && field.nr <= 18)
       .map((field) => field.nr),
   },
   high: {
-    payout: 1,
+    payout: 2,
     winningRolls: fields
       .filter((field) => field.nr >= 19 && field.nr <= 36)
       .map((field) => field.nr),
   },
   dozen1: {
-    payout: 2,
+    payout: 3,
     winningRolls: fields
       .filter((field) => field.nr >= 1 && field.nr <= 12)
       .map((field) => field.nr),
   },
   dozen2: {
-    payout: 2,
+    payout: 3,
     winningRolls: fields
       .filter((field) => field.nr >= 13 && field.nr <= 24)
       .map((field) => field.nr),
   },
   dozen3: {
-    payout: 2,
+    payout: 3,
     winningRolls: fields
       .filter((field) => field.nr >= 25 && field.nr <= 36)
       .map((field) => field.nr),
   },
-  ...fields.map((field) => {
-    return { payout: 35, winningRolls: [field.nr] };
-  }),
+  ...fields.reduce(
+    (acc, field) => ({
+      ...acc,
+      [field.nr]: { payout: 36, winningRolls: [field.nr] },
+    }),
+    { [fields[0].nr]: { payout: 36, winningRolls: [fields[0].nr] } },
+  ),
 };
+
+export function calculatePayout(
+  roll: number,
+  inputBets: { [key:string] : number },
+) {
+  let winnings = 0;
+  for (const [bet, amount] of Object.entries(inputBets)) {
+    if (!(bet in bets)) {
+      throw new Error(`Invalid bet type: ${bet}`);
+    }
+    if (bets[bet].winningRolls.includes(roll)) {
+      winnings += amount * bets[bet].payout;
+    }
+  }
+  return winnings;
+}
--- casino/app/src/routes/play/+page.server.ts
+++ casino/app/src/routes/play/+page.server.ts
@@ -4,31 +4,23 @@ import { db } from "$lib/server/db";
 import { lcg } from "$lib/server/db/schema";
 import { user } from "$lib/server/db/schema";
 import { eq } from "drizzle-orm";
+import { calculatePayout, bets as possibleBets } from "$lib/roulette";
 
 export const load: PageServerLoad = async (event) => {
   if (!event.locals.user) {
     return redirect(302, "/");
   }
 
-  // Initialize LCG parameters for the new user
-  // Randomize this eventually
-  /*
-	await db.insert(lcg).values({
-		lastResult: 1,
-		multiplier: 75,
-		increment: 0,
-		modulus: (2 ** 16)+1,
-		
-	});*/
-
+  /* load lcg parameters for user*/
   let lcgData = await db
     .select()
     .from(lcg)
     .where(eq(lcg.userId, event.locals.user.id));
 
+  /* if no lcg parameters exist for user, create default parameters */
   if (lcgData.length === 0) {
     await db.insert(lcg).values({
-      lastResult: 1,
+      lastResult: Math.floor(Math.random() * 2 ** 16 - 1),
       multiplier: 75,
       increment: 0,
       modulus: 2 ** 16 + 1,
@@ -36,30 +28,37 @@ export const load: PageServerLoad = async (event) => {
     });
   }
 
-  lcgData = await db
-    .select()
-    .from(lcg)
-    .where(eq(lcg.userId, event.locals.user.id));
-
-  return { user: event.locals.user, lcg: lcgData[0] };
+  return { user: event.locals.user };
 };
 
 export const actions = {
   default: async (event) => {
+    const formData = await event.request.formData();
+    /* load lcg data for user */
     const lcgData = await db
       .select()
       .from(lcg)
       .where(eq(lcg.userId, event.locals.user.id));
 
+    /* calculate next lcg result and corresponding roulette roll */
     const xn =
       (lcgData[0].lastResult * lcgData[0].multiplier + lcgData[0].increment) %
       lcgData[0].modulus;
 
+    const result = xn % 37;
+
+    /* update lcg data for user */
     await db
       .update(lcg)
       .set({ lastResult: xn })
       .where(eq(lcg.userId, event.locals.user.id));
 
-    return { result: "success", value: xn };
+    const bets : {[key:string] : number} = {};
+
+    Object.keys(possibleBets).forEach((bet) => {bets[bet] = +(formData.get(bet) || 0);});
+
+    const winnings = calculatePayout(result, bets);
+
+    return { result: "success", value: result, bets:bets, winnings: winnings };
   },
 } satisfies Actions;
